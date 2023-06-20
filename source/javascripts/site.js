54558b55d15a64cab7f4d84a6713448638a51185
joe weber <jweber4213@gmail.com>
2023-06-20 09:43:23 +0900

    added some pictures, reformatted projects & added links to res

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,19 +2,34 @@ const techDisp = document.querySelector('.tech-disp');
 console.log(techDisp);
 
 const techCards = [
-  '<i class="devicon-python-plain-wordmark"></i>',
-  '<i class="devicon-ruby-plain-wordmark "></i>',
-  '<i class="devicon-javascript-plain "></i>',
-  '<i class="devicon-html5-plain-wordmark "></i>',
-  '<i class="devicon-css3-plain-wordmark "></i>',
-  '<i class="devicon-postgresql-plain-wordmark "></i>',
-  '<i class="devicon-sqlite-plain-wordmark "></i>',
-  '<i class="devicon-django-plain-wordmark "></i>',
-  '<i class="devicon-rails-plain-wordmark "></i>',
-  '<i class="devicon-docker-plain-wordmark"></i>',
-  '<i class="devicon-github-original-wordmark"></i>',
-  '<i class="devicon-heroku-original-wordmark"></i>'
+  '<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />',
+  '<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original-wordmark.svg" />',
+  '<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />',
+  '<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />',
+  '<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />',
+  '<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />',
+  '<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original-wordmark.svg" />',
+  '<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain-wordmark.svg" />',
+  '<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-original-wordmark.svg" />',
+  '<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original-wordmark.svg" />',
+  '<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />',
+  '<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original-wordmark.svg" /> '
 ];
+
+// const techCards = [
+//   '<i class="devicon-python-plain-wordmark colored"></i>',
+//   '<i class="devicon-ruby-plain-wordmark colored"></i>',
+//   '<i class="devicon-javascript-plain colored"></i>',
+//   '<i class="devicon-html5-plain-wordmark colored"></i>',
+//   '<i class="devicon-css3-plain-wordmark colored"></i>',
+//   '<i class="devicon-postgresql-plain-wordmark colored"></i>',
+//   '<i class="devicon-sqlite-plain-wordmark colored"></i>',
+//   '<i class="devicon-django-plain-wordmark colored"></i>',
+//   '<i class="devicon-rails-plain-wordmark colored"></i>',
+//   '<i class="devicon-docker-plain-wordmark colored"></i>',
+//   '<i class="devicon-github-original-wordmark colored"></i>',
+//   '<i class="devicon-heroku-original-wordmark colored"></i>'
+// ];
 
 techCards.forEach(techCard => {
   const column = document.createElement('div');
